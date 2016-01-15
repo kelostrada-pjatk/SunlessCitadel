d3d75794c36a4ec018d256ed7540ea68226b00de
kelostrada <kelostrada@gmail.com>
2016-01-15 19:20:55 +0100

    added small joke to using rope.

--- logic/objects_usage.pl
+++ logic/objects_usage.pl
@@ -3,3 +3,22 @@ p_use(rope, pillars) :-
 	write(':)'),
 	nl,
 	!.
+	
+p_use(rope) :-
+	write('Hero doesn''t know what exactly to do with the rope so he starts using it like a hanging rope.'), nl,
+    write('  _____.------[_]'), nl,
+    write(' +------------|x|'), nl,
+    write(' |            |x|'), nl,
+    write(' |            |x|'), nl,
+    write(' |            |x|'), nl,
+    write(' |            |x|'), nl,
+    write(' j            |x|'), nl,
+    write(''' \\/\\_        |X|'), nl,
+    write('  (           |X|'), nl,
+    write(' (o)          |X|'), nl,
+    write('              |X|'), nl,
+    write('    \\         FXJ'), nl,
+    write('     z!      J>X<L'), nl,
+    write('            /_>x<_\\'), nl,
+    write('           []     []'), nl,
+	!.
